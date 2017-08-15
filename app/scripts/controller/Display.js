b90e069d7dae4818e5aef0d78f81cf99dbf721a1
GREGORY ASSASIE <gregjarvez@gmail.com>
2017-08-15 20:14:28 +0100

    html code generation complete.

--- app/scripts/controller/Display.js
+++ app/scripts/controller/Display.js
@@ -1,34 +1,1 @@
-import React from "react";
-
-
-class Display extends React.Component {
-    constructor(props) {
-        super(props);
-
-        
-    }
-
-
-    render() {
-        let {components} = this.props;
-        let list = components.map((Component, index) => {
-           return (
-              <Component.embeddedComponent
-                  key={index}
-                  config={Component.configuration}
-                  id={Component.id}
-                  del={this.props.del}
-              />
-           )
-        });
-        
-        return (
-            <div className="display">
-                {list}
-            </div>
-        );
-    }
-}
-
-
-export default Display;
+import React from "react";class Display extends React.Component {  constructor ( props ) {    super(props);  }  render () {    let { components } = this.props;    let list = components.map(( Component, index ) => {      return (        <Component.embeddedComponent          key={ index }          config={ Component.configuration }          id={ Component.id }          del={ this.props.del }        />      )    });    return (      <div className="display">        { list }      </div>    );  }}export default Display;
